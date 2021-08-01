e03f2e92bfee93848d0d04a325fe789f412dfa4d
Rob Gllett <therobgillett@gmail.com>
2021-08-01 13:30:37 -0700

    Initial Commit

---
+++ Shared/AppReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppReviewApp.swift
+//  Shared
+//
+//  Created by Robert Gillett on 8/1/21.
+//
+
+import SwiftUI
+
+@main
+struct AppReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
